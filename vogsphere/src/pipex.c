883bac969bce0b37db0b15da18c0cf68498ede4b
aLeuleu <aurelien.levra@gmail.com>
2023-01-20 15:33:42 +0100

    _

--- vogsphere/src/pipex.c
+++ vogsphere/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 16:19:25 by alevra            #+#    #+#             */
-/*   Updated: 2022/12/29 16:21:36 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2023/01/19 21:49:26 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	main(void)
 {
-	ft_printf("Hello\n");
+	ft_printf("Quand on veut transmettre une information via un pipe, il faut ouvrir le fd en entree et surtout, fermer le fd en sortie.
+	Ensuite, pour lire cette information, il faut fermer le fd de lecture et ouvrir le fd decriture\n");
 	return (0);
 }
